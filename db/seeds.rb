98983f6576a705d1f34ed62aec60fcd841f2174e
Addison Martin <15254821+addisonmartin@users.noreply.github.com>
2022-08-01 16:02:50 -0400

    Update views futher

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(email: 'richard@email.com', password: 'password', password_confirmation: 'password',
+             confirmed_at: Time.current)
+
+User.create!(email: 'collen@email.com', password: 'password', password_confirmation: 'password',
+             confirmed_at: Time.current)
+
+a = User.create!(email: 'addisonejmartin@gmail.com', password: 'password', password_confirmation: 'password',
+                 confirmed_at: Time.current)
+
+Page.create!(user: a, title: 'About', subtitle: 'About our Organization', published_at: Time.current,
+             body: 'We are Ottawa Food Rescue.')
+
+Page.create!(user: a, title: 'Donate', subtitle: 'How to Help our Community', published_at: Time.current,
+             body: 'We support: ...')
+
+Page.create!(user: a, title: 'Contact', subtitle: 'Get in touch!', published_at: Time.current,
+             body: 'We would love to hear from you!')
+
+Article.create!(user: a, title: "Ottawa Food Rescue's offical website launches!", published_at: Time.current,
+                body: "Please join us in celebrating the launch of our offical, dedicated website! \
+                This is only the beginning of the expansion of our online presence, check back \
+                later for more. Thank you.")
