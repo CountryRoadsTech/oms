c69ba8c7a338827ddac60396abe12f4ce5237869
Addison Martin <15254821+addisonmartin@users.noreply.github.com>
2022-08-04 13:02:44 -0400

    Fix master.key not being uploaded to deployment server

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,8 @@ set :branch, :main
 
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
-# Default value for linked_dirs is []
+append :linked_files, 'config/master.key'
+
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/uploads', 'vendor',
        'storage'
 
@@ -28,3 +29,15 @@ set :keep_assets, 10
 
 # Disable db:console push actions
 set :disallow_pushing, true
+
+namespace :deploy do
+  namespace :check do
+    before :linked_files, :set_master_key do
+      on roles(:app), in: :sequence, wait: 10 do
+        unless test("[ -f #{shared_path}/config/master.key ]")
+          upload! 'config/master.key', "#{shared_path}/config/master.key"
+        end
+      end
+    end
+  end
+end
