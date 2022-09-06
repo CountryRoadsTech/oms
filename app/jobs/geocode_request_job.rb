e464bedaec3ae715680263621e4db42bd278f19f
Addison Martin <15254821+addisonmartin@users.noreply.github.com>
2022-09-06 09:36:47 -0400

    Configure background jobs to check in with Honeybadger

--- app/jobs/geocode_request_job.rb
+++ app/jobs/geocode_request_job.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'net/http'
+require 'uri'
+
 # This job gets the best known location for a request's IP address
 class GeocodeRequestJob < ApplicationJob
   queue_as :default
@@ -11,5 +14,9 @@ class GeocodeRequestJob < ApplicationJob
     request.latitude = results.first.coordinates.first
     request.longitude = results.first.coordinates.second
     request.save!
+
+    # Check in with Honeybadger to let it know the job queue is still up and running jobs..
+    uri = URI.parse(Rails.application.credentials.honeybadger.sidekiq_checkin_url)
+    Net::HTTP.get_response(uri)
   end
 end
